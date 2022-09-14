867bced2282c5d8ec4077de05b0efd4cdb21de81
Tricia Amheiser <tricia@taarts.net>
2022-09-14 16:06:53 -0400

    1st eg. employees better to make the 2nd eg. 'betterArray'

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,21 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+// <----- Arrays ----->
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+console.clear()
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const array = [1, 2, 3, 4, 5, 'chris']
+console.log(array)
+
+const employees: string[] = []
+
+employees[0] = 'Shaky, Sue'
+employees[1] = 'Jumpy, John'
+employees[25] = 'Jolly, Jane'
+
+const betterArray = ['Shaky, Sue', 'Jumpy, John', 'Jolly, Jane']
+
+betterArray.push('Happy, Harry')
+
+console.log(betterArray)
+
+console.log(employees)
