d95e42e9c2905e0e395860261f74bf60b9937ae5
Tricia Amheiser <tricia@taarts.net>
2022-09-26 15:48:18 -0400

    Part 2 of Intro to Ts

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ employees[25] = 'Jolly, Jane'
 const betterArray = ['Shaky, Sue', 'Jumpy, John', 'Jolly, Jane']
 
 betterArray.push('Happy, Harry')
+// betterArray.shift('Happy, Harry')
 
 console.log(betterArray)
 
@@ -34,6 +35,7 @@ const colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple']
 // }
 
 colors.forEach((color) => console.log(color))
+// _________________________________________________
 
 let favoriteNumber: 13 | 42
 {
@@ -127,11 +129,77 @@ function printIt(numbers: number[], func: printItFunction) {
   }
 }
 
+// Alert
+// function AlertIt(numbers: number[], func: PrintItFunction) {
+//   for (let index = 0; index < numbers.length; index++) {
+//     const value = numbers[index]
+//     const result = func(value)
+
+//     window.alert(`Turned ${value} into ${result}`)
+//   }
+// }
+
 const numbers = [1, 2, 3, 4, 5]
 printIt(numbers, square)
 printIt(numbers, double)
 // this  vvv would be an error
 printIt(numbers, makeSomething)
 
-const words = ['hello', 'world']
-printIt(words, upperCase)
+// const words = ['hello', 'world']
+// printIt(words, upperCase)
+
+//------- CLOSURES - interview question
+// Closures in Typescript are a way to create a function that has access to the variables and functions defind in the outer scope.
+const variableFromOuterScope = "Wow, I'm from the outer scope"
+function thisFunctionActsLikeAClosure() {
+  const variableFromInnerScope = 42
+  console.log(
+    `I'm a closure! I have access to the variable "${variableFromOuterScope}" and the variable "${variableFromInnerScope}"`
+  )
+  // Have the debugger stop the program so we can look around
+  debugger
+}
+thisFunctionActsLikeAClosure()
+
+const people = [
+  {
+    name: 'Alan Turing',
+    birthDate: 'June 23, 1912',
+    delayMilliseconds: 1100,
+  },
+  {
+    name: 'Ada Lovelace',
+    birthDate: 'December 10, 1815',
+    delayMilliseconds: 1500,
+  },
+  {
+    name: 'Grace Hopper',
+    birthDate: 'December 9, 1906',
+    delayMilliseconds: 2000,
+  },
+  {
+    name: 'Donald Knuth',
+    birthDate: 'January 10, 1938',
+    delayMilliseconds: 2500,
+  },
+]
+
+function printPersonInfo(person) {
+  console.log(`${person.name} was born on ${person.birthDate}`)
+}
+people.forEach(function (person) {
+  // Inside here we have access to the `person` variable. The `person` variable is
+  // recreated each time through the forEach loop. Since it is an argument to the
+  // function, it is a new creation of that variable.
+  function timeoutCallback() {
+    // We also have access to the `person` variable here. The function timeoutCallback
+    // will remember all the variables that existed that it could see. This includes
+    // the `person` variable from the forEach loop.
+    // When the function executes **later** (via the `setTimeout`) the
+    // fact that this is a "closure" (remembers variables from the outer scope) ensures
+    // that each time we call printPersonInfo, the variable `person` will be correct.
+    debugger
+    printPersonInfo(person)
+  }
+  setTimeout(timeoutCallback, person.delayMilliseconds)
+})
